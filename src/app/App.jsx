65f78afda778f0d151b694b951da52ef2e5687a4
Oscar Paramo <oscar.paramo@me.com>
2019-06-30 19:55:23 -0500

    Typescript is disappointing :(

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import './App.css';
 import Explorer from './components/explorer/Explorer';
+import Dashboard from './components/dashboard/Dashboard';
 
-const App = () => (<Explorer />);
+
+const App = () => (<Dashboard />);
 
 export default App;
